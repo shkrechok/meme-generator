51a2ac2badbc5ea99211acece7830666c0f0f524
Marina Milman <shkrechok@gmail.com>
2023-04-27 14:09:08 +0300

    switching sections css continue

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -10,6 +10,22 @@ function onInit() {
     galleryController.onInit()
 }
 
+function onNavTo(elNavTo) {
+    if (elNavTo.classList.contains('active')) return
+    const elNavToData = elNavTo.dataset.section
+    const elNavItems = document.querySelectorAll('.main-nav-item')
+    const elMainContentItems = document.querySelectorAll('.main-item')
+    elNavItems.forEach(elNavItem => elNavItem.classList.remove('active'))
+    elMainContentItems.forEach(elMainContentItem => {
+        if (!elMainContentItem.classList.contains('hidden')) 
+        elMainContentItem.classList.add('hidden')
+    })
+    elNavTo.classList.add('active')
+    const classToCheck = elNavToData
+    const elMainContentToShow = document.querySelector(`.${classToCheck}`)
+    elMainContentToShow.classList.remove('hidden')
+}
+
 
 
 
